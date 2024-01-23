c532038422367e1b7bab70101beca369dd8f3cf6
Shouq <shougsam@gmail.com>
2024-01-23 09:18:23 +0300

    change 2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from io import StringIO
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, jsonify, json, send_file
+import requests
 import csv
 from datetime import datetime
 import pandas as pd
 from docx import Document
 from flask_cors import CORS
+from docx2pdf import convert
 
 '''
 'StringIO' To provide a file-like object interface to the CSV data stored in the 
@@ -18,6 +20,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/api/generate_report", methods=['POST'])
 
 def generate_report():
@@ -26,7 +33,7 @@ def generate_report():
        report_type = request.form.get('report_type')
        client = request.form.get('client')
        date = request.form.get('date')
-       #csv_file = request.files.get('csv_file')
+       csv_file = request.files.get('csv_file')
        #pdf_file = request.files.get('pdf_file') if report_type == 'monthly' else None
 
      # Check if required fields are missing
@@ -37,64 +44,70 @@ def generate_report():
          missing_fields.append("Client")
        if date is None:
          missing_fields.append("Date")
- 
+
+       if csv_file is None or not allowed_file(csv_file.filename):
+         missing_fields.append("CSV File is missing or has an invalid extension.")
+
        if missing_fields:
-         raise ValueError("Missing required fields:", missing_fields)
-    
-       #if report_type.lower() == "weekly" and (csv_file is None or not csv_file.filename.lower().endswith('.csv')):
-        # raise ValueError("CSV file is required")
-    
-       report_info = {
-            "Report Type": report_type,
-            "Client": client,
-            "Date": date,
-           # "csv file": csv_file.filename if csv_file else None,
-           }
-    
-       return jsonify({"Report Information": report_info}), 200
-    
+         raise ValueError("Missing required fields: " + ", ".join(missing_fields))
+       
+       json_data = csv_to_json(csv_file)
+
+      # Print the JSON data to the console
+       print(json_data) 
+
+       document = generate_word(report_type, client, date, json_data)
+
+      # Save the Word document with a filename based on client name and selected date
+       word_file = f"{report_type} {client} {date}.docx"
+       document.save(word_file)
+
+       pdf_file = f"{report_type} {client} {date}.pdf"
+       convert(word_file, pdf_file)
+
+      # send the file
+       return send_file(word_file, as_attachment=True), send_file(pdf_file, as_attachment=True)
+
+
     except Exception as e:
         error_message = str(e)
         return jsonify({"error": error_message}), 400
-    
-#     def read_csv(csv_file):
-#     csv_data = []
-#     csv_reading = csv.DictReader(csv_file)
-#     for row in csv_reading:
-#         csv_data.append(row)
-#         json_csv = json.dumps(csv_data)
-#     return json_csv
-# def generate_word(json_csv):
-#     template_path = ''
-#     doc = Document(template_path)
-    
-#     file_path = ''
-#     csv_file.save(file_path)
+     
 
-#     if 'csv_file' not in request.files:
-#         return jsonify({'error': 'No file part'})
-#     if csv_file.filename == '':
-#         return jsonify({'error': 'No selected file'})
-#     if csv_file:
-#         # Read the CSV file from the in-memory file-like object
-#         csv_content = csv_file.read().decode('utf-8')
-        
-#         # Check if the file is empty
-#         if not csv_content:
-#             return jsonify({'error': 'Empty file'})
-#         data = pd.read_csv(StringIO(csv_content))
-    
-#         # Example: Convert the DataFrame to JSON and print it        
+def csv_to_json(csv_file):
+    data = []
 
-#     # Check file types
-#    # if report_type == 'weekly' and csv_file and csv_file.filename.endswith('.csv'):
-        
-#      #   word_file = weekly_report(data_js, client, date, report_type)
-#     #elif report_type == 'monthly' and csv_file and pdf_file and csv_file.filename.endswith('.csv') and pdf_file.filename.endswith('.pdf'):
-       
-#        # word_file = monthly_report(data_js, pdf_file, client, date)
-#    # else:
-#         #return jsonify({'error': 'Invalid file type'}), 400
+    with open(csv_file, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data.append(row)
+
+    return json.dumps(data, indent=2)
+
+
+
+def generate_word(report_type, client, date, json_data):
+   document  = Document("template.docx")
+   return
+
+def AbuseIPDB(IP):
+    url = 'https://api.abuseipdb.com/api/v2/check'
+
+    querystring = {
+        'ipAddress':IP,
+        'maxAgeInDays':'300',
+    }
+
+    headers = {
+        'Accept': 'application/json',
+        'Key': 'a583db48791dbdabf54a208dd89bdadc2bcd127c98d69fa45f9037ed965dd672f06bfd9d1db63be1'
+    }
+
+    response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+
+    # Formatted output
+    decodedResponse = json.loads(response.text)
+    return ((decodedResponse["data"]))
    
 app.run(host="0.0.0.0", port=5000, debug=True)
 
